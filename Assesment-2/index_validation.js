4845d729a4c280b12fec33d19593b7d5dca44937
bhavinpatel2911 <112382640+bhavinpatel2911@users.noreply.github.com>
2023-03-28 21:21:25 +0530

    Update index_validation.js

--- Assesment-2/index_validation.js
+++ Assesment-2/index_validation.js
@@ -2,14 +2,17 @@ function validation_check(){
     var username=document.getElementById('username').value;
     var password=document.getElementById('password').value;
     var letter=/^\S*$/;
+    //Validation for Password
     if(username == "" || !letter.test(username)){
         document.getElementById('username_msg').innerHTML="*Username Requires For Login";
         return false;
     }
+    //Validation for Password
     if(username.length<=2 || username.length>=10){
         document.getElementById('username_msg').innerHTML="*Please Enter Valid Username";
         return false;
-    } 
+    }
+    //Validation for Password
     if(password == ""){
         document.getElementById('pass_msg').innerHTML="*Password Requires For Login";
         return false;
